b22bfa4456c1179ed0a22521f9a72797b0526d90
Priyanshu <patelpriyanshu649@gmail.com>
2021-11-23 08:20:57 +0530

    Remove EXE Files

---
+++ Function_Template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+So we can just specify datatype in
+main() function so it saves us time 
+by not writing same function again
+& again like function Overloading.
+*/
+
+template <typename T>
+T add(T x,T y){
+    return (x+y);
+}
+
+int main(){
+    cout<<"Addition of 5 & 15 :- "<<add<int>(5,15);
+    cout<<"\nAddition of 5.5 & 15.5 :- "<<add<float>(5.5,15.5);
+    return 0;
+}
