1ae2bdb0af4d2ac095d57ee4251da438c6628bf1
Priyanshu Dholu <patelpriyanshu649@gmail.com>
2022-02-11 21:51:59 +0530

    Added Description

--- Concepts/Pure_Virtual_Function.cpp
+++ Concepts/Pure_Virtual_Function.cpp
@@ -6,8 +6,26 @@ We have to initialize The Function with 0
 to make it Pure Virtual Function.
 
 Also If We Add Arguments in Functions 
-in derived classthen it will do function overriding.
+in derived class then it will do function overriding.
 */
+
+/*
+Pure Virtual Functions & Abstract Class in C++ :-
+
+Sometimes implementation of all function cannot be provided in a base class
+because we don’t know the implementation. Such a class is called abstract class.
+
+A pure virtual function (or abstract function) in C++ is a virtual function
+for which we don’t have implementation, we only declare it.
+
+A pure virtual function is declared by assigning 0 in declaration. 
+Some important facts – 
+1) A class is abstract if it has at least one pure virtual function.
+2) We can have pointers and references of abstract class type.
+3) If we do not override the pure virtual function in derived class, then derived class also becomes abstract class.
+4) Abstract classes cannot be instantiated. 
+*/
+
 
 class Shape
 {
